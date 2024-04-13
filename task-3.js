7b3054503c0422c3fb1a4fda7f01098f948183d1
Elina Zaitseva <elinochkat932@gmail.com>
2024-04-13 20:05:33 +0300

    add file js2

--- task-3.js
+++ task-3.js
@@ -1,8 +1,10 @@
+
+
 function getElementWidth (content, padding, border) {
-const boxSizing = Number.parseFloat(content) + (Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2);
-return boxSizing;
+return Number.parseInt(content) + (Number.parseInt(padding) * 2 + Number.parseFloat(border) * 2);
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); 
 console.log(getElementWidth("60px", "12px", "8.5px")); 
 console.log(getElementWidth("200px", "0px", "0px"));
+
